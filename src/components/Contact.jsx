b4805a6fb076438d26bc247c85d005558fed1d62
Rahil Patel <Rahil.Patel1715@gmail.com>
2023-12-12 14:10:09 -0500

    added send email functionalities

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,19 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_iz5e9v1',
+        'template_75n3hgh',
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Rahil Patel",
           from_email: form.email,
           to_email: "sujata@jsmastery.pro",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'Kt2ceqyhs6tviOPeb'
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
