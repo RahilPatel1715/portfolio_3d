b52a105175153242c5eb1279c8043a2044117a6a
Rahil Patel <Rahil.Patel1715@gmail.com>
2023-12-12 13:37:09 -0500

    Checkin

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -8,54 +8,59 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
+import sql from "./tech/sql.png"
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
+import csharp from "./tech/csharp.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import aspnet from "./tech/aspnet.png";
+import bs from "./tech/bootstrap.png";
+import jira from "./tech/Jira.png";
+import sNow from "./tech/snow.png"
+// import heroImage from "./heroimage.png"
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import asrm from "./company/ASRM.png"
+import comcast from "./company/Comcast.png"
+import govCanada from "./company/GovernmentOfCanada.png"
+import ibm from "./company/IBM.png"
+import zonic from "./company/Zonic.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  asrm,
+  comcast,
+  govCanada,
+  ibm,
+  bs,
+  jira,
+  sNow,
+  zonic,
   logo,
   backend,
   creator,
   mobile,
   web,
+  sql,
+  csharp,
+  aspnet,
   github,
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-const About = () => {
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full ">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()} className="text-center">
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <div className="flex justify-center">
+        <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-center">
+          A passionate front-end developer with over six years of hands-on experience crafting delightful and responsive user interfaces. My journey in the world of web development has been driven by a deep curiosity for creating seamless digital experiences. I am excited about the opportunity to bring my expertise to new challenges and contribute to creating exceptional digital experiences. Let's build something amazing together!
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex justify-center flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
