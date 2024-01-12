72fd49ad2977213e19229f4db31f81c6027efee6
Rahil Patel <Rahil.Patel1715@gmail.com>
2024-01-12 01:40:42 -0500

    Added most recent exp.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,18 @@ const About = () => {
 
       <div className="flex justify-center">
         <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-center">
-          A passionate front-end developer with over six years of hands-on experience crafting delightful and responsive user interfaces. My journey in the world of web development has been driven by a deep curiosity for creating seamless digital experiences. I am excited about the opportunity to bring my expertise to new challenges and contribute to creating exceptional digital experiences. Let's build something amazing together!
+          With over 5 years of Front-End development and proficient in web technologies like HTML, CSS/CSS3, jQuery, Bootstrap, XML, JavaScript, Tailwind CSS, Material UI and JSON essential for front-end development.  <br />
+          <hr className=" py-4 border-0" />
+          Demonstrated ability to use JSON for storing and transporting data between server and client in a human-readable format. <br />
+          <hr className=" py-4 border-0" />
+
+          Excellent knowledge of JavaScript, CSS3/CSS, HTML5/HTML, Bootstrap, Tailwind CSS, Material UI, JavaScript and their behavioral on different browsers. Expertise in React JS framework to develop the SPA. <br />
+          <hr className=" py-4 border-0" />
+
+          Solid experience developing the rendering view React JS components, forms, events, routers, and Redux asynchronous functions and implemented React - Flux pattern for core dependency. <br />
+          <hr className=" py-4 border-0" />
+
+          Well-versed in methodologies such as Agile, Waterfall and SCRUM, allowing for a flexible approach to project management.
         </motion.p>
       </div>
 
